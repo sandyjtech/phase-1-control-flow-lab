89be07ddb84f3bdfc643432f192f710d336a9f7f
Sandra G <sandyjtech@gmail.com>
2023-06-11 16:20:59 -0700

    added functions with conditions

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet >= 400 && feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (feet <= 2001 && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+ternaryCheckCity("NYC");
+ternaryCheckCity("Pittsburgh");
+
+function switchOnCharmFromTip(tip) {
+  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+
+    case "not as generous":
+      return "Thank you.";
+
+    default:
+      return "Bye.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip("generous");
+switchOnCharmFromTip("not as generous");
+switchOnCharmFromTip("thanks for everything");
